fb8ad62a7140f14a353cb454f64805d3d7bec10b
Nimesha Buddhika <135071J@uom.lk>
2018-05-01 02:14:44 +0530

    ngIf

--- Demo/src/app/app.component.ts
+++ Demo/src/app/app.component.ts
@@ -1,27 +1,33 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({ // Metadata , Decorator 
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
 export class AppComponent {  //Class component 
-  title = 'Nimesha';
-  siteUrl = window.location.href;
-  className = "green-text";
-  msg = "";
+    title = 'Nimesha';
+    siteUrl = window.location.href;
+    className = "green-text";
+    msg = "";
+    isVisible = false;
 
-  public name = "";
+    public name = "";
 
-  getName(){
-    return "Nimesha Buddhika";
-  }
-  onClick(event){
-    console.log(event);
-    this.msg = "Hello";
-  }
+    getName() {
+        return "Nimesha Buddhika";
+    }
 
-  onLog(userName){
-    alert(userName);
-  }
+    onClick(event) {
+        console.log(event);
+        this.msg = "Hello";
+    }
+
+    onLog(userName) {
+        alert(userName);
+    }
+
+    onShow() {
+        this.isVisible = !this.isVisible;
+    }
 }
