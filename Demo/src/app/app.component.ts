c324bdd84d20c301dcc327b177d0f870cf877b64
Nimesha Buddhika <135071J@uom.lk>
2018-04-30 18:06:19 +0530

    test component added

--- Demo/src/app/app.component.ts
+++ Demo/src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 
-@Component({
+@Component({ // Metadata , Decorator 
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent {  //Class component 
+  title = 'Hello app';
 }
